674d9ad420f595435aed81243c6637ca3e7d0b11
Marian13 <mariankostyk13895@gmail.com>
2022-11-14 21:22:52 +0200

    feat(user): generate initial devise user

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  ##
+  # Includes default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise \
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   root "home#index"
+
+  devise_for :users
 end
