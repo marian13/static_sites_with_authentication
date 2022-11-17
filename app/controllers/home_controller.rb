89f710465288ffc57eeb3982af6786d823897cee
Marian13 <mariankostyk13895@gmail.com>
2022-11-17 22:07:06 +0200

    feat(root): redirect to rules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@
 class HomeController < ApplicationController
   ##
   # Processes application root.
+  # https://api.rubyonrails.org/v7.0.4/classes/ActionController/Redirecting.html#method-i-redirect_to
   #
   def index
+    redirect_to "/rules"
   end
 end
